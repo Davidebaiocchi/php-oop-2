aed97705c885712b671126249cb36aa98727ca87
davide <davide.baiocchi97@gmail.com>
2021-09-02 17:06:13 +0200

    fine grafica

--- index.php
+++ index.php
@@ -28,121 +28,116 @@ require_once __DIR__ . '/hp7b.php';
     <title>Harry Potter Saga</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
-
-<header>
-
-</header>
 
 <body>
     <h1>Saga di Harry Potter</h1>
+    <main>
 
-    <div class="card">
-        <img src=" <?php echo $hp1->img ?> " alt="">
-        <div class="description">
-            <h2 class='title'> <?php echo $hp1->title ?> </h2>
-            <p> <?php echo $hp1->genre ?> / <?php echo $hp1->date_drop ?> </p>
-            <p class="trama"> <?php echo $hp1->trama ?> </p>
-            <p class="price"> <?php echo $hp1->price ?> </p>
-            <p class="trama"> <?php foreach ( $hp1->cast as $key => $value ) {
-                        echo $key ." - ".$value . '<br>';
-                    } ?>
-            </p> 
-        </div>
-    </div>
-    <div class="card">
-        <img src=" <?php echo $hp2->img ?> " alt="">
-        <div class="description">
-            <h2 class='title'> <?php echo $hp2->title ?> </h2>
-            <p> <?php echo $hp2->genre ?> / <?php echo $hp2->date_drop ?> </p>
-            <p class="trama"> <?php echo $hp2->trama ?> </p>
-            <p class="price"> <?php echo $hp2->price ?> </p>
-            <p class="trama"> <?php foreach ( $hp2->cast as $key => $value ) {
-                        echo $key ." - ".$value . '<br>';
-                    } ?>
-            </p> 
-        </div>
-    </div>
-    <div class="card">
-        <img src=" <?php echo $hp3->img ?> " alt="">
-        <div class="description">
-            <h2 class='title'> <?php echo $hp3->title ?> </h2>
-            <p> <?php echo $hp3->genre ?> / <?php echo $hp3->date_drop ?> </p>
-            <p class="trama"> <?php echo $hp3->trama ?> </p>
-            <p class="price"> <?php echo $hp3->price ?> </p>
-            <p class="trama"> <?php foreach ( $hp3->cast as $key => $value ) {
-                        echo $key ." - ".$value . '<br>';
-                    } ?>
-            </p> 
-        </div>
-    </div>
-    <div class="card">
-        <img src=" <?php echo $hp4->img ?> " alt="">
-        <div class="description">
-            <h2 class='title'> <?php echo $hp4->title ?> </h2>
-            <p> <?php echo $hp4->genre ?> / <?php echo $hp4->date_drop ?> </p>
-            <p class="trama"> <?php echo $hp4->trama ?> </p>
-            <p class="price"> <?php echo $hp4->price ?> </p>
-            <p class="trama"> <?php foreach ( $hp4->cast as $key => $value ) {
-                        echo $key ." - ".$value . '<br>';
-                    } ?>
-            </p> 
-        </div>
-    </div>
-    <div class="card">
-        <img src=" <?php echo $hp5->img ?> " alt="">
-        <div class="description">
-            <h2 class='title'> <?php echo $hp5->title ?> </h2>
-            <p> <?php echo $hp5->genre ?> / <?php echo $hp5->date_drop ?> </p>
-            <p class="trama"> <?php echo $hp5->trama ?> </p>
-            <p class="price"> <?php echo $hp5->price ?> </p>
-            <p class="trama"> <?php foreach ( $hp5->cast as $key => $value ) {
-                        echo $key ." - ".$value . '<br>';
-                    } ?>
-            </p> 
-        </div>
-    </div>
-    <div class="card">
-        <img src=" <?php echo $hp6->img ?> " alt="">
-        <div class="description">
-            <h2 class='title'> <?php echo $hp6->title ?> </h2>
-            <p> <?php echo $hp6->genre ?> / <?php echo $hp6->date_drop ?> </p>
-            <p class="trama"> <?php echo $hp6->trama ?> </p>
-            <p class="price"> <?php echo $hp6->price ?> </p>
-            <p class="trama"> <?php foreach ( $hp6->cast as $key => $value ) {
-                        echo $key ." - ".$value . '<br>';
-                    } ?>
-            </p> 
-        </div>
-    </div>
-    <div class="card">
-        <img src=" <?php echo $hp7a->img ?> " alt="">
-        <div class="description">
-            <h2 class='title'> <?php echo $hp7a->title ?> </h2>
-            <p> <?php echo $hp7a->genre ?> / <?php echo $hp7a->date_drop ?> </p>
-            <p class="trama"> <?php echo $hp7a->trama ?> </p>
-            <p class="price"> <?php echo $hp7a->price ?> </p>
-            <p class="trama"> <?php foreach ( $hp7a->cast as $key => $value ) {
-                        echo $key ." - ".$value . '<br>';
-                    } ?>
-            </p> 
-        </div>
-    </div>
-    <div class="card">
-        <img src=" <?php echo $hp7b->img ?> " alt="">
-        <div class="description">
-            <h2 class='title'> <?php echo $hp7b->title ?> </h2>
-            <p> <?php echo $hp7b->genre ?> / <?php echo $hp7b->date_drop ?> </p>
-            <p class="trama"> <?php echo $hp7b->trama ?> </p>
-            <p class="price"> <?php echo $hp7b->price ?> </p>
-            <p class="trama"> <?php foreach ( $hp7b->cast as $key => $value ) {
-                        echo $key ." - ".$value . '<br>';
-                    } ?>
-            </p> 
-        </div>
-    </div>
-</body>
 
-    
+        <div class="card">
+            <img src=" <?php echo $hp1->img ?> " alt="">
+            <div class="description">
+                <h2 class='title'> <?php echo $hp1->title ?> </h2>
+                <p> <?php echo $hp1->genre ?> / <?php echo $hp1->date_drop ?> </p>
+                <p class="trama"> <?php echo $hp1->trama ?> </p>
+                <p class="price"> <?php echo $hp1->price ?> </p>
+                <p class="trama"> <?php foreach ( $hp1->cast as $key => $value ) {
+                            echo $key ." - ".$value . '<br>';
+                        } ?>
+                </p> 
+            </div>
+        </div>
+        <div class="card">
+            <img src=" <?php echo $hp2->img ?> " alt="">
+            <div class="description">
+                <h2 class='title'> <?php echo $hp2->title ?> </h2>
+                <p> <?php echo $hp2->genre ?> / <?php echo $hp2->date_drop ?> </p>
+                <p class="trama"> <?php echo $hp2->trama ?> </p>
+                <p class="price"> <?php echo $hp2->price ?> </p>
+                <p class="trama"> <?php foreach ( $hp2->cast as $key => $value ) {
+                            echo $key ." - ".$value . '<br>';
+                        } ?>
+                </p> 
+            </div>
+        </div>
+        <div class="card">
+            <img src=" <?php echo $hp3->img ?> " alt="">
+            <div class="description">
+                <h2 class='title'> <?php echo $hp3->title ?> </h2>
+                <p> <?php echo $hp3->genre ?> / <?php echo $hp3->date_drop ?> </p>
+                <p class="trama"> <?php echo $hp3->trama ?> </p>
+                <p class="price"> <?php echo $hp3->price ?> </p>
+                <p class="trama"> <?php foreach ( $hp3->cast as $key => $value ) {
+                            echo $key ." - ".$value . '<br>';
+                        } ?>
+                </p> 
+            </div>
+        </div>
+        <div class="card">
+            <img src=" <?php echo $hp4->img ?> " alt="">
+            <div class="description">
+                <h2 class='title'> <?php echo $hp4->title ?> </h2>
+                <p> <?php echo $hp4->genre ?> / <?php echo $hp4->date_drop ?> </p>
+                <p class="trama"> <?php echo $hp4->trama ?> </p>
+                <p class="price"> <?php echo $hp4->price ?> </p>
+                <p class="trama"> <?php foreach ( $hp4->cast as $key => $value ) {
+                            echo $key ." - ".$value . '<br>';
+                        } ?>
+                </p> 
+            </div>
+        </div>
+        <div class="card">
+            <img src=" <?php echo $hp5->img ?> " alt="">
+            <div class="description">
+                <h2 class='title'> <?php echo $hp5->title ?> </h2>
+                <p> <?php echo $hp5->genre ?> / <?php echo $hp5->date_drop ?> </p>
+                <p class="trama"> <?php echo $hp5->trama ?> </p>
+                <p class="price"> <?php echo $hp5->price ?> </p>
+                <p class="trama"> <?php foreach ( $hp5->cast as $key => $value ) {
+                            echo $key ." - ".$value . '<br>';
+                        } ?>
+                </p> 
+            </div>
+        </div>
+        <div class="card">
+            <img src=" <?php echo $hp6->img ?> " alt="">
+            <div class="description">
+                <h2 class='title'> <?php echo $hp6->title ?> </h2>
+                <p> <?php echo $hp6->genre ?> / <?php echo $hp6->date_drop ?> </p>
+                <p class="trama"> <?php echo $hp6->trama ?> </p>
+                <p class="price"> <?php echo $hp6->price ?> </p>
+                <p class="trama"> <?php foreach ( $hp6->cast as $key => $value ) {
+                            echo $key ." - ".$value . '<br>';
+                        } ?>
+                </p> 
+            </div>
+        </div>
+        <div class="card">
+            <img src=" <?php echo $hp7a->img ?> " alt="">
+            <div class="description">
+                <h2 class='title'> <?php echo $hp7a->title ?> </h2>
+                <p> <?php echo $hp7a->genre ?> / <?php echo $hp7a->date_drop ?> </p>
+                <p class="trama"> <?php echo $hp7a->trama ?> </p>
+                <p class="price"> <?php echo $hp7a->price ?> </p>
+                <p class="trama"> <?php foreach ( $hp7a->cast as $key => $value ) {
+                            echo $key ." - ".$value . '<br>';
+                        } ?>
+                </p> 
+            </div>
+        </div>
+        <div class="card">
+            <img src=" <?php echo $hp7b->img ?> " alt="">
+            <div class="description">
+                <h2 class='title'> <?php echo $hp7b->title ?> </h2>
+                <p> <?php echo $hp7b->genre ?> / <?php echo $hp7b->date_drop ?> </p>
+                <p class="trama"> <?php echo $hp7b->trama ?> </p>
+                <p class="price"> <?php echo $hp7b->price ?> </p>
+                <p class="trama"> <?php foreach ( $hp7b->cast as $key => $value ) {
+                            echo $key ." - ".$value . '<br>';
+                        } ?>
+                </p> 
+            </div>
+        </div>
+    </main> 
 </body>
 </html>
